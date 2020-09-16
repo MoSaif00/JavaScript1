814ab1e7104f69658aec5a9613a414431940695e
Mohammed Saif <69274977+MoSaif00@users.noreply.github.com>
2020-09-16 17:06:34 +0200

    I update the code to be specific value
    i had defined numberX at first as nothing which it has to be undefined specifically

--- Week1/homework/js-exercises/LogTheNumber.js
+++ Week1/homework/js-exercises/LogTheNumber.js
@@ -1,7 +1,7 @@
 "use strict";
 let numberX;
-console.log("Ithink the value of x is nothing");
+console.log("Ithink the value of x is going to be undefined value ");
 console.log(numberX);
 numberX = 4;
-console.log(" Ithink the number of x is 4");
+console.log(" the number of x is 4 after declaring the value .");
 console.log(numberX);
