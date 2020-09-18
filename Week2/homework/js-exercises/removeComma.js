62d2fe28093fe029c5cc956df84febb4b9288899
MoSaif00 <moh97ab@hotmail.com>
2020-09-19 00:57:42 +0200

    removed all commas from file removeComma

---
+++ Week2/homework/js-exercises/removeComma.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Remove a commas from a  string 
+
+/*
+- Add the variable to your file.
+- Log the length of myString.
+- The commas make that the sentence is quite hard to read. Find a 
+way to remove the commas from the string and replace them with spaces. 
+- After replacing the commas, log myString to see if you succeeded.
+*/
+
+// the string needs to be commas removed 
+let myString = 'hello,this,is,a,difficult,to,read,sentence';
+
+console.log(myString.length);
+
+// logging mystring with replacing comma by space
+console.log(myString.replace(/,/g," "));
