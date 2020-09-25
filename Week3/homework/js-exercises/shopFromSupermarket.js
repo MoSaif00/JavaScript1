eb3b2919d6ce45f297e9264402a3ed9ddd0c2b84
MoSaif00 <moh97ab@hotmail.com>
2020-09-26 00:05:03 +0200

    finished Shopping at supermarket list program and already tested it

---
+++ Week3/homework/js-exercises/shopFromSupermarket.js
@@ -0,0 +1,45 @@
+'use strict ';
+
+// App when you have more than 3 items in your shopping cart the first item gets taken out.
+/*
+1 - Create an array called shoppingCart that holds the following strings: "bananas" and "milk"
+2 - Write a function named addToShoppingCart
+- It takes 1 argument: a grocery item (string)
+- Add grocery item to shoppingCart. If the amount of items is more than 3 remove the first one in the array
+- Loops through the array in order to list out the items
+- Return a string: "You bought [LIST_OF_GROCERY_ITEMS]!"
+3 - Call the function 3 times, each time with a different string as the argument
+*/
+
+const shoppingCart = [
+    'bananas',
+    'milk'
+]; // declared array with name of items 
+
+function addToShoppingCart(item){
+    shoppingCart.push(item); // to push the new item to the list
+
+    if(shoppingCart.length > 3){  
+        shoppingCart.shift();
+    } // check if the the list more than 3 items  remove one from the beggining 
+
+   /* 
+    for (let i = 0; i < shoppingCart.length; i++) {
+        console.log(shoppingCart[i]);
+      } // for-loop to go through the array to list the items 
+    
+    */
+    console.log(` You bought ${shoppingCart.join(', ')}.`);
+    
+}
+
+addToShoppingCart('chocolate'); // returns bananas , milk, chocolate 
+addToShoppingCart('Waffles'); //returns milk, chocolate , Waffles
+addToShoppingCart('tea'); // returns chocolate, Waffles, tea
+
+
+// // This is another way to loop three times with adding new items from an array 
+// const theNewItems = ['chocolate', 'Waffles', 'Tea'];
+// for (let i = 0; i < 3; i++) {
+//   addToShoppingCart(theNewItems[i]);
+// }
