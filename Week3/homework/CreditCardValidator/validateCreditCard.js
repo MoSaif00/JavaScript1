bde420c9f1f3b20165351a258147d7ea693e8ade
MoSaif00 <moh97ab@hotmail.com>
2020-09-27 20:52:25 +0200

    working on credit card validator

---
+++ Week3/homework/CreditCardValidator/validateCreditCard.js
@@ -0,0 +1,12 @@
+'use strict ';
+
+// In this project you'll write a function, called validateCreditNumber, that validates whether or not a credit card number is valid.
+
+// Here are the criteria for a valid number:
+
+// Input must be 16 characters
+// All characters must be numbers
+// At least two different numbers should be represented
+// The last number must be even
+// The sum of all the numbers must be greater than 16
+
